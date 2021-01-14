aa056ec523393d4a4aa38d8a71b496f753482f8e
mlongano <mauro.longano@marconirovereto.it>
2021-01-14 22:01:29 +0000

    Async-await error handling

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 async function myFetch () {
-    let response = await fetch( 'coffee.jpg' );
-    if ( !response.ok ) {
-        throw new Error( `HTTP error! status: ${response.status}` );
-    } else {
-        return await response.blob();
+    try {
+        let response = await fetch( 'coffee.jpg' );
+
+        if ( !response.ok ) {
+            throw new Error( `HTTP error! status: ${response.status}` );
+        } else {
+            let myBlob = await response.blob();
+            let objectURL = URL.createObjectURL( myBlob );
+            let image = document.createElement( 'img' );
+            image.src = objectURL;
+            document.body.appendChild( image );
+        }
+    } catch ( e ) {
+        console.log( e );
     }
 }
 
-myFetch().then( ( blob ) => {
-    let objectURL = URL.createObjectURL( blob );
-    let image = document.createElement( 'img' );
-    image.src = objectURL;
-    document.body.appendChild( image );
-} ).catch( e => console.log( e ) );
+myFetch();
