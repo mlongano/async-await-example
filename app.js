f17ffdaf1a3abd9579d424c9afc89fe8ff39ca63
mlongano <mauro.longano@marconirovereto.it>
2021-01-14 21:52:46 +0000

    Promises esempio completo

--- app.js
+++ app.js
@@ -1,17 +1,45 @@
-fetch( 'coffee.jpg' )
-    .then( response => {
+function fetchAndDecode ( url, type ) {
+    return fetch( url ).then( response => {
         if ( !response.ok ) {
             throw new Error( `HTTP error! status: ${response.status}` );
         } else {
-            return response.blob();
+            if ( type === 'blob' ) {
+                return response.blob();
+            } else if ( type === 'text' ) {
+                return response.text();
+            }
         }
     } )
-    .then( myBlob => {
-        let objectURL = URL.createObjectURL( myBlob );
-        let image = document.createElement( 'img' );
-        image.src = objectURL;
-        document.body.appendChild( image );
-    } )
-    .catch( e => {
-        console.log( 'There has been a problem with your fetch operation: ' + e.message );
-    } );
+        .catch( e => {
+            console.log( `There has been a problem with your fetch operation for resource "${url}": ` + e.message );
+        } )
+        .finally( () => {
+            console.log( `fetch attempt for "${url}" finished.` );
+        } );
+}
+
+let coffee = fetchAndDecode( 'coffee.jpg', 'blob' );
+let tea = fetchAndDecode( 'tea.jpg', 'blob' );
+let description = fetchAndDecode( 'description.txt', 'text' );
+
+Promise.all( [ coffee, tea, description ] ).then( values => {
+    console.log( values );
+    // Store each value returned from the promises in separate variables; create object URLs from the blobs
+    let objectURL1 = URL.createObjectURL( values[ 0 ] );
+    let objectURL2 = URL.createObjectURL( values[ 1 ] );
+    let descText = values[ 2 ];
+
+    // Display the images in <img> elements
+    let image1 = document.createElement( 'img' );
+    let image2 = document.createElement( 'img' );
+    image1.src = objectURL1;
+    image2.src = objectURL2;
+    document.body.appendChild( image1 );
+    document.body.appendChild( image2 );
+
+    // Display the text in a paragraph
+    let para = document.createElement( 'p' );
+    para.textContent = descText;
+    document.body.appendChild( para );
+
+} );
